98ce69f8363be12faca45f57a6b7bbf2ea30d0df
Kieran Elmes <aiftw84@gmail.com>
2017-08-15 13:54:52 +1200

    fixed an issue where xinput commands would not have access to xinput, removed broken notify-osd

--- yoga-tabletmode.sh
+++ yoga-tabletmode.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 # for some reason disabling the trackpoint enables the touchpad, make sure to disable the trackpoint first.
 
+# adjust to fit, apparently necessary for access to xinput
+export DISPLAY=:0
+export XAUTHORITY=/home/kieran/.Xauthority
+
 case "$1" in
 	disable)
 		xinput enable "SynPS/2 Synaptics TouchPad"
 		xinput enable "TPPS/2 IBM TrackPoint"
-		notify-send "Laptop Mode"
 		;;
 	enable)
 		xinput disable "TPPS/2 IBM TrackPoint"
 		xinput disable "SynPS/2 Synaptics TouchPad"
-		notify-send "Tablet Mode"
 		;;
 esac
